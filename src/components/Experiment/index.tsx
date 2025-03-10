5b9c2245cb3bd5f4b249bdd854214729767de5ac
Gabriel <alecksandrov15@gmail.com>
2025-03-10 19:16:20 +0300

    chore: clearChartData on unmount; disabling button on start

--- src/components/Experiment/index.tsx
+++ src/components/Experiment/index.tsx
@@ -4,6 +4,7 @@ import { generateDefiniteSequence, generateNumberSequence } from '@/utils';
 import { sleep, waitForSpecificKey } from '@/utils/experiment';
 import { useExperimentActions } from '@/store/experiment';
 import { Keyboard } from '@/components/Keyboard';
+import { useEffect, useState } from 'react';
 import { Chart } from '@/components/Chart';
 
 interface IExperimentProps {
@@ -15,28 +16,36 @@ interface IExperimentProps {
 const DEFINITE_VALUE = 5;
 
 export function Experiment({ keyboardType, isRandom }: IExperimentProps) {
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
   const { setCurrentDigit, clearCurrentDigit, appendChartData, clearChartData } =
     useExperimentActions();
 
   async function handleDynamicSequence(sequence: Array<string>) {
-    clearChartData();
-    for (const [index, key] of sequence.entries()) {
-      setCurrentDigit(key);
-      const startTime = Date.now();
-      const pressedKey = await waitForSpecificKey(key);
-      const endTime = Date.now();
-      const diffTime = endTime - startTime;
-      const time = diffTime / (60 * 60);
-      const spm = Math.round((index + 1) / time);
-      const chartData = { name: (index + 1).toString(), time: spm };
+    try {
+      setIsDisabled(true);
+      clearChartData();
+      for (const [index, key] of sequence.entries()) {
+        setCurrentDigit(key);
+        const startTime = Date.now();
+        const pressedKey = await waitForSpecificKey(key);
+        const endTime = Date.now();
+        const diffTime = endTime - startTime;
+        const time = diffTime / (60 * 60);
+        const spm = Math.round((index + 1) / time);
+        const chartData = { name: (index + 1).toString(), time: spm };
 
-      appendChartData(chartData);
+        appendChartData(chartData);
+        clearCurrentDigit();
+        console.log(`Шаг ${index + 1}: Нажата ${pressedKey}`);
+        await sleep(1000);
+      }
       clearCurrentDigit();
-      console.log(`Шаг ${index + 1}: Нажата ${pressedKey}`);
-      await sleep(1000);
+      console.log('Все клавиши введены правильно!');
+    } catch (error) {
+      console.log('error ', error);
+    } finally {
+      setIsDisabled(false);
     }
-    clearCurrentDigit();
-    console.log('Все клавиши введены правильно!');
   }
 
   const handleStart = async () => {
@@ -47,11 +56,17 @@ export function Experiment({ keyboardType, isRandom }: IExperimentProps) {
     await handleDynamicSequence(_sequence);
   };
 
+  useEffect(() => {
+    return () => clearChartData();
+  }, [clearChartData]);
+
   return (
     <div>
       <h1>Keyboard (1)</h1>
       <Keyboard type={keyboardType} />
-      <button onClick={handleStart}>Запустить</button>
+      <button onClick={handleStart} disabled={isDisabled}>
+        Запустить
+      </button>
       <Chart />
     </div>
   );
